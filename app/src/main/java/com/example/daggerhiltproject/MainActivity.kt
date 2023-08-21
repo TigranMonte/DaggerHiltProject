9cc1041dd9955e507c987ca609998b649a98bc1a
TigranMonte <tigranma@gmail.com>
2023-08-21 22:00:44 +0300

    Dagger Hilt Project init commit

--- app/src/main/java/com/example/daggerhiltproject/MainActivity.kt
+++ app/src/main/java/com/example/daggerhiltproject/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var wifiManager: WifiManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = WiFiSettings()
+        wifiManager = WifiManager(settings)
+
+        wifiManager.connect()
+        wifiManager.sendMessage()
     }
 }
--- app/src/main/java/com/example/daggerhiltproject/WifiManager.kt
+++ app/src/main/java/com/example/daggerhiltproject/WifiManager.kt
@@ -1,4 +1,21 @@
 package com.example.daggerhiltproject
 
-class WifiManager {
+import android.util.Log
+
+class WifiManager(private val settings: WiFiSettings) {
+    fun connect(){
+        settings.openConnection()
+    }
+    fun sendMessage(){
+        settings.writeBytes()
+    }
+}
+
+class WiFiSettings{
+    fun openConnection(){
+        Log.d("MyLog", "Connected")
+    }
+    fun writeBytes(){
+        Log.d("MyLog", "Hola!")
+    }
 }
