3607b3f27c8674e02010ff7abf13df32baf2d74f
TigranMonte <tigranma@gmail.com>
2023-09-24 17:04:12 +0300

    Dagger Hilt Project (ViewModel)

--- app/src/main/java/com/example/daggerhiltproject/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/daggerhiltproject/fragments/FirstFragment.kt
@@ -12,7 +12,6 @@ import com.example.daggerhiltproject.WiFiManager
 import com.example.daggerhiltproject.databinding.FragmentFirstBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-
 @AndroidEntryPoint
 class FirstFragment : Fragment() {
     @Inject
@@ -23,15 +22,14 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -41,6 +39,7 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
