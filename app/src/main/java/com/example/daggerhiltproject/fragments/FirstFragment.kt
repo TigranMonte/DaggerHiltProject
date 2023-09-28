da0a2544d9b3b1a9db8a246656f761b256f037e9
TigranMonte <tigranma@gmail.com>
2023-09-29 00:03:01 +0300

    Dagger Hilt Project (ViewModel)

--- app/src/main/java/com/example/daggerhiltproject/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/daggerhiltproject/fragments/FirstFragment.kt
@@ -18,8 +18,10 @@ class FirstFragment : Fragment() {
     lateinit var wifiManager: WiFiManager
 
     private var _binding: FragmentFirstBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
+
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
